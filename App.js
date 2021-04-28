5bb5ad1c9498c710fbab0597a47f20484567c58b
Basavachetan GM <chetan@revolotech.com>
2021-04-28 12:56:32 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { Home } from './screens/'
+
+const theme = {
+  ...DefaultTheme,
+  color: {
+    ...DefaultTheme.colors,
+    border: "transparent"
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName={''}
+        >
+        <Stack.Screen  name="Home" component={Home} />
+
+        </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App
